3b5086cd0a31760c1e64b19a91a5f81647a4c718
Daniele Faugiana <wifau@me.com>
2012-12-28 01:21:48 +0100

    Pushed to GitHub

--- larrybird/settings.py
+++ larrybird/settings.py
@@ -73,7 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ROOTPATH + '/static_dirs',
+    #ROOTPATH + '/static_dirs',
 )
 
 # List of finder classes that know how to find static files in
